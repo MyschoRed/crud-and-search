09243dc7e10a75e4fd8f1e27c48128fa66f693d3
Michal Spisiak <97632552+MyschoRed@users.noreply.github.com>
2023-07-17 08:22:01 +0200

    change search and added class based view

--- core/views.py
+++ core/views.py
@@ -35,8 +35,12 @@ def index(request):
             messages.success(request, 'Student deleted successfully')
 
         elif 'search' in request.POST:
-            query = request.POST.get('searchquery')
-            students = Student.objects.filter(Q(name__icontains=query) | Q(email__icontains=query))
+            if request.POST.get('searchqueryname'):
+                query = request.POST.get('searchqueryname')
+                students = Student.objects.filter(Q(name__icontains=query))
+            elif request.POST.get('searchqueryemail'):
+                query = request.POST.get('searchqueryemail')
+                students = Student.objects.filter(Q(email__icontains=query))
 
 
     ctx = {
@@ -44,3 +48,58 @@ def index(request):
         'query': query
     }
     return render(request, 'index.html', ctx)
+
+# Class Based View alternative
+# from django.views.generic import TemplateView
+# class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        students = Student.objects.all()
+        query = ''
+        context['students'] = students
+        context['query'] = query
+        return context
+
+    def post(self, request, *args, **kwargs):
+        students = Student.objects.all()
+        query = ''
+
+        if 'add' in request.POST:
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            Student.objects.create(name=name, email=email)
+            messages.success(request, "Študent úspešne pridaný")
+
+        elif 'update' in request.POST:
+            id = request.POST.get('id')
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+
+            update_student = Student.objects.get(id=id)
+            update_student.name = name
+            update_student.email = email
+            update_student.save()
+
+            messages.success(request, 'Študent úspešne aktualizovaný')
+
+        elif 'delete' in request.POST:
+            id = request.POST.get('id')
+            Student.objects.get(id=id).delete()
+
+            messages.success(request, 'Študent úspešne vymazaný')
+
+        elif 'search' in request.POST:
+            if request.POST.get('searchqueryname'):
+                query = request.POST.get('searchqueryname')
+                students = Student.objects.filter(Q(name__icontains=query))
+            elif request.POST.get('searchqueryemail'):
+                query = request.POST.get('searchqueryemail')
+                students = Student.objects.filter(Q(email__icontains=query))
+
+        context = {
+            'students': students,
+            'query': query
+        }
+        return self.render_to_response(context)
